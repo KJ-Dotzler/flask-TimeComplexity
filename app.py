e87f0d4e1096602daf33e134709ce92609f2e477
KJ-Dotzler <kjdotzler@mccneb.edu>
2025-11-04 20:27:09 +0000

    added data app route in app py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
+import os
 
 #create a flask object/instance
 app = Flask(__name__)
@@ -10,6 +11,14 @@ app = Flask(__name__)
 #select which html page to render when app runs
 def index():
     return render_template('index.html')
+
+@app.route('/data')
+def data():
+    dataPath = os.path.join(app.root_path, 'complexities.json')
+    with open(dataPath, 'r') as dataIn:
+        jsonOut = json.load(dataIn)
+    return jsonify(jsonOut)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
