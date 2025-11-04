cae2520667b6d7107e9d79d697c196ed06e8020b
KJ-Dotzler <kjdotzler@mccneb.edu>
2025-11-04 20:05:43 +0000

    updated base and index html, added sytling to CSS, created main js, added json import to app py, updated json, removed chart/scripts js, updated base html script tag to correct CDM

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import Flask, render_template
+import json
+
+#create a flask object/instance
+app = Flask(__name__)
+
+#give the url/route, since it is first run use /
+@app.route('/')
+
+#select which html page to render when app runs
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/JS/main.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", async() => {
+    const dropdown = document.getElementById("algorithm");
+    const algoName = document.getElementById("algoName");
+    const algoComplex = document.getElementById("algoComplex");
+    const ctx = document.getElementById("complexChart").getContext("2d");
+
+    const response = await fetch("/data");
+    const data = await response.json();
+    const algorithms = data.algorithms;
+
+    algorithms.forEach(algo => {
+        const option = document.createElement("option");
+        option.value = algo.name;
+        option.textContent = algo.name;
+        dropdown.appendChild(option);
+    });
+    const chart = new Chart(ctx, {
+        type : "line",
+        data : {
+            labels : Array.from({length: 10}, (_,i) => i + 1),
+            datasets: []
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {beginAtZero: true, title: {display: true, text: "Operations"}},
+                x: {title: {display: true, text: 'Input Size (n)'}}
+            }
+        }
+    })
+
+    dropdown.addEventListener('change', (e) => {
+        const selected = algorithms.find(a => a.name === e.target.value);
+        algoName.textContent = selected.name;
+        algoComplex.innerHTML = `Time Complexity: <strong>${selected.complexity}</strong>`;
+
+        const growth = simulateComplexity(selected.complexity);
+        chart.data.datasets = [{
+            label: selected.name,
+            data: growth,
+            borderColor: '#36a2eb',
+            tension: 0.2
+        }];
+        chart.update();
+    });
+});
+
+function simulateComplexity(type){
+    const nVals = Array.from({length: 10}, (_,i) => i + 1);
+    switch (type) {
+        case 'O(1)': return nVals.map(() => 1);
+        case 'O(log n)': return nVals.map(n => Math.log2(n));
+        case 'O(n)': return nVals.map(n => n);
+        case 'O(n log n)': return nVals.map(n => n * Math.log2(n));
+        case 'O(n^2)': return nVals.map(n => n * n);
+        default: return nVals.map(() => 0);
+    }
+}
